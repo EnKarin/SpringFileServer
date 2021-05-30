6e9eefdbe0cc812bca7380835a30d79aabee8f53
Карина Елагина <karina.elagina2013@yandex.ru>
2021-05-30 15:30:17 +0700

    Download via URL available

--- src/main/java/com/fileServer/FileServer/Controller/DownloadsController.java
+++ src/main/java/com/fileServer/FileServer/Controller/DownloadsController.java
@@ -1,35 +1,85 @@
 package com.fileServer.FileServer.Controller;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLConnection;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/download")
+public class DownloadsController {
+
+    private static final String EXTERNAL_FILE_PATH = "D:/IdeaProgects/FileServer/Save/";
+
+    @RequestMapping("/file/{fileName:.+}")
+    public void downloadPDFResource(HttpServletRequest request, HttpServletResponse response,
+                                    @PathVariable("fileName") String fileName) throws IOException {
+
+        File file = new File(EXTERNAL_FILE_PATH + fileName);
+        if (file.exists()) {
+
+            String mimeType = URLConnection.guessContentTypeFromName(file.getName());
+            if (mimeType == null) {
+                //unknown mimetype so set the mimetype to application/octet-stream
+                mimeType = "application/octet-stream";
+            }
+
+            response.setContentType(mimeType);
+            response.setHeader("Content-Disposition", String.format("inline; filename=\"" + file.getName() + "\""));
+            response.setContentLength((int) file.length());
+
+            InputStream inputStream = new BufferedInputStream(new FileInputStream(file));
+            FileCopyUtils.copy(inputStream, response.getOutputStream());
+        }
+    }
+}
+
+/*
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class DownloadsController {
-    @RequestMapping(path = "download", method=RequestMethod.GET)
-    public ResponseEntity<Object> downloadFile() throws IOException  {
-        String filename = "/var/tmp/mysql.png";
-        File file = new File(filename);
-        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
-        HttpHeaders headers = new HttpHeaders();
 
-        headers.add("Content-Disposition", String.format("attachment; filename=\"%s\"", file.getName()));
-        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
-        headers.add("Pragma", "no-cache");
-        headers.add("Expires", "0");
+    @RequestMapping(value = "/*", method=RequestMethod.GET)
+    public  @ResponseBody ResponseEntity<Object> downloadFile(@RequestParam("name") String name,
+                                                              @RequestParam("file") MultipartFile files) throws IOException  {
+        String fileName = Objects.requireNonNull(file.getOriginalFilename());
+        File result = new File("D://IdeaProgects//FileServer//Save", fileName);
+        HttpHeaders header = new HttpHeaders();
+        header.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=img.jpg");
+        header.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        header.add("Pragma", "no-cache");
+        header.add("Expires", "0");
 
-        ResponseEntity<Object>
-                responseEntity = ResponseEntity.ok().headers(headers).contentLength(
-                file.length()).contentType(MediaType.parseMediaType("application/txt")).body(resource);
+        Path path = Paths.get(result.getAbsolutePath());
+        ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
 
-        return responseEntity;
+        return ResponseEntity.ok()
+                .headers(header)
+                .contentLength(result.length())
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .body(resource);
     }
 }
+*/
